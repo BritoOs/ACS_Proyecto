861637b8f6fc2a72b335c5a3b27d89f703ca65c9
BritoOs <santi.brito123@gmail.com>
2022-10-30 17:40:32 -0600

    add sign in to teams route

--- backend/src/controllers/teamscontroller.js
+++ backend/src/controllers/teamscontroller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const pool = require('../conexion')
 
 async function allteamsquery(req, res) {
-    const sql = 'SELECT * FROM TEAMS'
+    const sql = 'SELECT T.TEAM_ID, T.TEAMNAME, T.FOR_BETTING, T.PRICE, M.USER_ID FROM TEAMS AS T LEFT JOIN USERS_MEMBERSHIP_PAYMENT AS M ON T.TEAM_ID = M.TEAM_ID ORDER BY T.TEAM_ID'
      
     pool.query(sql)
         .then(data => {
@@ -21,4 +21,14 @@ async function newteam(req, res) {
         })
 }
 
-module.exports = {allteamsquery, newteam}
+async function signintoteam(req, res) {
+    const sql = 'INSERT INTO USERS_MEMBERSHIP_PAYMENT (USER_ID, TEAM_ID) VALUES ('+ req.body.user_id + ', ' + req.body.team_id + ')'
+    console.log(sql)
+    pool.query(sql)
+        .then(data => {
+            console.log(data);
+            res.send(data.rows);
+        })
+}
+
+module.exports = {allteamsquery, newteam, signintoteam}
--- backend/src/routes/teamsroutes.js
+++ backend/src/routes/teamsroutes.js
@@ -5,5 +5,6 @@ const queries = require('../controllers/teamscontroller')
 
 router.get('/teams', queries.allteamsquery)
 router.post('/newteam', queries.newteam)
+router.post('/signintoteam', queries.signintoteam)
 
 module.exports = router
